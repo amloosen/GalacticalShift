1ed884a45820847a2793d8f9b797686f6cda3107
Alisa Loosen <a.m.loosen@gmail.com>
2021-09-15 16:02:19 +0100

    added timer to elements

--- src/Components/elementsHover.js
+++ src/Components/elementsHover.js
@@ -9,7 +9,6 @@ import Blue from "./img/stimuli3_blue.jpg";
 import Red from "./img/stimuli3_red.jpg";
 import Green from "./img/stimuli3_green.jpg";
 import Yellow from "./img/stimuli3_yellow.jpg";
-import Hello from "./Hello";
 
 import "./style/barstyles.css";
 //
@@ -24,8 +23,8 @@ class ElementsTraining extends React.Component{
 
     /* data to be saved .*/
   var times_element1 = Array(1).fill().map(() => Array(3).fill(0));
-  // var times_element2 = Array(trial_per_block).fill().map(() => Array(3).fill(0));
-  // var times_element3 = Array(trial_per_block).fill().map(() => Array(3).fill(0));
+  var times_element2 = Array(1).fill().map(() => Array(3).fill(0));
+  var times_element3 = Array(1).fill().map(() => Array(3).fill(0));
 
 
   this.state  = {
@@ -39,8 +38,8 @@ class ElementsTraining extends React.Component{
     show2: null,
     show3: null,
     times_element1: times_element1,
-    // times_element2: times_element2,
-    // times_element3: times_element3
+    times_element2: times_element2,
+    times_element3: times_element3
   };
 
   this.mouseOver = this.mouseOver.bind(this);
@@ -54,39 +53,47 @@ class ElementsTraining extends React.Component{
   });
   }
 
-  mouseOver() {
-         this.state.times_element1.push([Math.round(performance.now(),0,0]);
+  mouseOver(elNr) {
+    if (elNr === 1){
+         this.state.times_element1.push([Math.round(performance.now()),0,0]);
          this.setState({
            img1: Blue,
            show1: 1,
-           showtime1: Math.round(performance.now()),
-         });
-debugger;
+         });} else if (elNr === 2){
+        this.state.times_element2.push([Math.round(performance.now()),0,0]);
+        this.setState({
+          img2: Red,
+          show2: 1,
+        });} else if (elNr === 3){ debugger;
+       this.state.times_element3.push([Math.round(performance.now()),0,0]);
+       this.setState({
+         img3: Yellow,
+         show3: 1,
+       });}
+
      }
 
-  mouseOut() {
-    this.state.times_element1[this.state.times_element1.length-1][1] =Math.round(performance.now();
-    // this.state.times_element1[this.state.times_element1.length-1][3] = this.state.times_element1[this.state.times_element1.length-1][0]-this.state.times_element1[this.state.times_element1.length-1][1];
-
-    debugger;
-    // ([this.state.showtime1,Math.round(performance.now()),this.state.showtime1-Math.round(performance.now())]
-        // times_element1[trialNum][1] = Math.round(performance.now());
-        // times_element1[trialNum][2] = times_element1[trialNum][1]-times_element1[trialNum][0] ;
+     mouseOut(elNr) {
+       if (elNr === 1){
+         this.state.times_element1[this.state.times_element1.length-1][1] =Math.round(performance.now());
+         this.state.times_element1[this.state.times_element1.length-1][2] = this.state.times_element1[this.state.times_element1.length-1][1]-this.state.times_element1[this.state.times_element1.length-1][0];
          this.setState({
            img1: Cover,
            show1: null
-         });
-
-     }
-
-  // componentDidMount() {
-  //     setInterval(() => {
-  //       this.setState(prevState => ({
-  //         progress:
-  //           (prevState.progress + Math.floor(Math.random() * 20) + 1) % 100
-  //       }));
-  //     }, 1000);
-  //   }
+         });} else if (elNr === 2){
+           this.state.times_element2[this.state.times_element2.length-1][1] =Math.round(performance.now());
+           this.state.times_element2[this.state.times_element2.length-1][2] = this.state.times_element2[this.state.times_element2.length-1][1]-this.state.times_element2[this.state.times_element2.length-1][0];
+           this.setState({
+             img2: Cover,
+             show2: null
+           });} else if (elNr === 3){debugger;
+             this.state.times_element3[this.state.times_element3.length-1][1] =Math.round(performance.now());
+             this.state.times_element3[this.state.times_element3.length-1][2] = this.state.times_element3[this.state.times_element3.length-1][1]-this.state.times_element3[this.state.times_element3.length-1][0];
+             this.setState({
+               img3: Cover,
+               show3: null
+             });}
+         }
 
   render() {
     return (<div className={styles.cockpit}>
@@ -95,20 +102,8 @@ debugger;
       <View style={styles.container}>
       <img className={styles.elementsize }
       src={this.state.img1}
-      onMouseOver={this.mouseOver}
-      onMouseOut={this.mouseOut}
-      // onMouseOver={() => {this.setState({
-      //   img1: Blue,
-      //   show1: 1
-      // });
-      // }}
-      // onMouseOut={() => {
-        // this.setState({
-        //   img1: Cover,
-        //   show1: null
-        // });
-      // }
-
+      onMouseOver={(elNr) => this.mouseOver(1)}
+      onMouseOut={(elNr) => this.mouseOut(1)}
       />
       {this.state.show1? <div className={styles.overlay}>
       <ElementBar progress={this.state.progress1} />
@@ -120,18 +115,8 @@ debugger;
       <View style={styles.container}>
       <img className={styles.elementsize }
       src={this.state.img2}
-      onMouseOver={() => {
-        this.setState({
-          img2: Red,
-          show2: 1
-        });
-      }}
-      onMouseOut={() => {
-        this.setState({
-          img2: Cover,
-          show2: null
-        });
-      }}
+      onMouseOver={(elNr) => this.mouseOver(2)}
+      onMouseOut={(elNr) => this.mouseOut(2)}
       />
       {this.state.show2? <div className={styles.overlay}>
       <ElementBar progress={this.state.progress2} />
@@ -141,20 +126,10 @@ debugger;
       </View>
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       <View style={styles.container}>
-      <img className={styles.elementsize }
+      <img className={styles.elementsize}
       src={this.state.img3}
-      onMouseOver={() => {
-        this.setState({
-          img3: Yellow,
-          show3: 1
-        });
-      }}
-      onMouseOut={() => {
-        this.setState({
-          img3: Cover,
-          show3: null
-        });
-      }}
+      onMouseOver={(elNr) => this.mouseOver(3)}
+      onMouseOut={(elNr) => this.mouseOut(3)}
       />
       {this.state.show3? <div className={styles.overlay}>
       <ElementBar progress={this.state.progress3}/>
