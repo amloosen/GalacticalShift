e1ca9fc1298eb8f9c0f20d5d68ddcd90a835fa89
Alisa Loosen <a.m.loosen@gmail.com>
2021-10-22 11:14:07 -0400

    Update MainTask.js

--- src/Components/MainTask.js
+++ src/Components/MainTask.js
@@ -365,7 +365,9 @@ class MainTask extends React.Component {
 
   handleOutcomeData = (height) => {
     var outcomeHeight_tmp = this.state.outcomeHeight;
-    outcomeHeight_tmp[this.state.trialNum - 1] = height;
+    var height_tmp = height;
+    height_round = height_tmp.toFixed(3);
+    outcomeHeight_tmp[this.state.trialNum - 1] = height_round;
     this.nextTrial(0, outcomeHeight_tmp);
   };
 
