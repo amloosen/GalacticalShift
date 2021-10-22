f1736e037677019b1d9fdb2e7c03e8a88074cda3
Alisa Loosen <a.m.loosen@gmail.com>
2021-10-21 23:43:54 -0400

    Update EndPage.js

--- src/Components/EndPage.js
+++ src/Components/EndPage.js
@@ -34,10 +34,8 @@ class EndPage extends React.Component {
     // This will change for the questionnaires going AFTER the main task
     this.state = {
       userID: this.props.location.state.userID,//debugger
-      // userID: 12, //debugger
-      // startTime: 1, //debugger
       startTime: this.props.location.state.startTime,//debugger
-      sectionStartTime: timeString, 
+      sectionStartTime: timeString,
       instructScreenText: 1,
       instructScreen: true,
       feedback: [],
