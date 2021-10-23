523c9021bc35c1b1a79c4900a25955824b7c36ce
Alisa Loosen <a.m.loosen@gmail.com>
2021-10-23 17:35:18 -0400

    include gif mouse hoover

--- src/Components/TrainingIntroA.js
+++ src/Components/TrainingIntroA.js
@@ -1,9 +1,14 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import GifPlayer from "react-gif-player";
+import "./style/gifplayer.scss";
 import { withRouter } from "react-router-dom";
 import styles from "./style/taskStyle.module.css";
 import img_intro1 from "./intro/ExamplePicture1.jpg";
 import img_left from "./intro/left.jpg";
 import img_right from "./intro/right.jpg";
+import gif from './intro/mouse.gif'
+import Blue from "./img/stimuli3_blue.jpg";
 /////////////////////////////////////////////////////////////////////////////////t.Component {
 class TrainingIntroA extends React.Component {
   constructor(props) {
@@ -49,9 +54,9 @@ class TrainingIntroA extends React.Component {
 
     if (whichButton === 4 && curText > 1) {
       this.setState({ instructScreenText: curText - 1 });
-    } else if (whichButton === 5 && curText < 6) {
+    } else if (whichButton === 5 && curText < 7) {
       this.setState({ instructScreenText: curText + 1 });
-    } else if (curText === 6 && whichButton === 10) {
+    } else if (curText === 7 && whichButton === 10) {
       setTimeout(
         function () {
           this.redirectToNextStage();
@@ -113,18 +118,17 @@ class TrainingIntroA extends React.Component {
               <br />
               Well done so far!
               <br /> <br />
-              We will now introduce you to the main task itself, letting you<br /><br />
-              complete few training trials.
+              We will now introduce you to the main task itself and you will
+              complete a few training trials.
               <br /> <br />
               For today&apos;s game, you will be a space explorer on an
-              intergalactic mission. <br />
-              <br />
-              Your mission is it to tell your station on earth of how many
-              aliens live on the planets you visit.
+              intergalactic mission. Your mission is it <br />
+              to tell your station on earth how many aliens live on the planets
+              you visit.
               <br />
               <br />
               It is critical that you give your best estimate of the alien
-              population size <br /><br />for the mission to be successful.
+              population size.
               <br /> <br />
               <span className={styles.center}>
                 <i>(Use the ← → keys to navigate the pages.)</i>
@@ -143,18 +147,25 @@ class TrainingIntroA extends React.Component {
               So how can you find out how many aliens live on a planet?
               <br />
               <br />
-              Your spaceship is equipped with several measuring instruments that
-              will help you determine <br />
+              You can find out how large the population size is by looking at
+              the different coloured resources
               <br />
-              how many aliens live on the planet.
+              available on the planet.
+              <br />
+              <br />
+              Your spaceship is equipped with several measuring instruments that
+              will help you determine
+              <br />
+              how much of each resource is available.
               <br />
               <br />A measuring instrument may look like this:
-              <br /><br />
+              <br />
+              <br />
               <span className={styles.center}>
                 <img src={img_intro1} alt="example1" />
               </span>
               <br />
-              This instrument indicates 40% of the resource is available.
+              This instrument indicates 40% of the blue resource is available.
               <br />
               <br />
               <span className={styles.center}>
@@ -169,17 +180,26 @@ class TrainingIntroA extends React.Component {
             <p>
               <span className={styles.center}>TRAINING II</span>
               <br />
-              The aliens living on these planets rely on natural resources,
-              <br /> <br />
+              As mentioned, the aliens living on these planets rely on natural
+              resources,
+              <br />
               and thus the population size is related to the measurement of your
               instrument.
               <br />
               <br />
-              For example, the reading of the measurement could be reflecting
-              the population size <br /><br />(in million) one-to-one. <br />
+              For example, the reading of the instrument could be reflecting the
+              population size <br />
+              (in million) one-to-one. This would mean that if the instrument
+              shows you ‘40%’ 40 million aliens <br />
+              live on that planet.
               <br />
-              This would mean that if the instrument shows you ‘40%’ 40 million
-              aliens live on that planet. <br />
+              <br />
+              It could also be, that the reading of the instrument could be
+              reflecting the population size <br />
+              times two. This would mean that if the instrument shows you ‘40%’
+              80 million aliens <br />
+              live on that planet. <br />
+              <br />
               <br />
               <span className={styles.center}>
                 [<strong>← BACK</strong>] [<strong>NEXT →</strong>]
@@ -193,21 +213,19 @@ class TrainingIntroA extends React.Component {
             <p>
               <span className={styles.center}>TRAINING II</span>
               <br />
-              However, a challenge makes your mission more difficult:
+              A challenge makes your mission difficult:
               <br />
               <br />
               No one knows how the instrument readings map onto the population
               size of the planet.
               <br />
-              <br />
               The association between the instrument reading and the population
               size <br />
+              will be <strong>complex</strong> and also <strong>change</strong>{" "}
+              at some point.
               <br />
-              will be <strong>more complex</strong> and also{" "}
-              <strong>change</strong> at some point.
               <br />
-              <br />
-              It is your task to learn the associations and keep track of
+              It is your task to learn the associations and keep track of the
               changes.
               <br /> <br />
               <span className={styles.center}>
@@ -222,23 +240,41 @@ class TrainingIntroA extends React.Component {
             <p>
               <span className={styles.center}>TRAINING II</span>
               <br />
-              For simplicity, we will now introduce you to the structure of the
-              game by
+              To introduce you to the game slowly, we will now introduce you to
+              the switching association
+              <br />
+              by, for now, only showing you the important instrument. <br />
+              <br />
+              This instrument is associated with the population size in a simple
+              way but this association will change <br />
+              at some point and you have to notice that and adapt your answer.
               <br />
               <br />
-              using one instrument with simple associations that will change at
-              some point.
+              First, look at the instrument by{" "}
+              <strong>hoovering over the black square with your mouse.</strong>
+              <br /> 
+               Click 'play' to see how.
+              <br />  <br />  <br />
+              <span className={styles.center}>
+              <GifPlayer gif={gif} still={Blue} />
+                </span>
+              <br />
+              <span className={styles.center}>
+                [<strong>← BACK</strong>] [<strong>NEXT →</strong>]
+              </span>
+            </p>
+          </div>
+        );
+      } else if (this.state.instructScreenText === 6) {
+        text = (
+          <div className={styles.main}>
+            <p>
+              <span className={styles.center}>TRAINING II</span>
               <br />
               <br />
-              We want to see whether you can detect the associations and the
-              changes.
-              <br /> <br />
-              First, look at the instrument by hoovering over the black square
-              with your mouse. <br />
-              <br />
-              Second, choose between the two alternative population sizes <br />
-              <br />
-              on the screen using the corresponding arrow keys.
+              You then have to choose between the two alternative population sizes on the
+              screen using the <br />
+              corresponding arrow keys.
               <br /> For the option on the left side use the left arrow key
               <img
                 className={styles.introImgTwo2}
@@ -261,21 +297,20 @@ class TrainingIntroA extends React.Component {
             </p>
           </div>
         );
-      } else if (this.state.instructScreenText === 6) {
+      } else if (this.state.instructScreenText === 7) {
         text = (
           <div className={styles.main}>
             <p>
               <span className={styles.center}>TRAINING II</span>
               <br />
-              For your first training, we will show you how many aliens actually
-              lived on the planet <br />
+              After you indicated your answer, we will show you how many aliens
+              actually lived on the planet.
               <br />
-              after you indicated your answer.
+              This will make it possible for you to learn how the instrument
+              relates to
+              the alien population size.
               <br />
               <br />
-              This will make it possible for you to find out how the instrument
-              relates to <br /><br />the alien population size.
-              <br /> <br />
               Let's practice this!
               <br /> <br />
               <span className={styles.center}>
