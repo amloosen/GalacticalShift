69545876b2b36409f1d90f4fa9767eca386fa1fb
Alisa Loosen <a.m.loosen@gmail.com>
2021-10-25 00:43:39 -0400

    refine feedback

--- src/Components/DisplayFeedback.js
+++ src/Components/DisplayFeedback.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 import styles from "./style/taskStyle.module.css";
-import { StyleSheet, View } from "react-native-web";
-import OutcomeSlider from "./SliderOutcome";
+import { StyleSheet, View, Text } from "react-native-web";
 import OutcomeSliderBar from "./SliderOutcomeBar";
 
 class DispFeedback extends React.Component {
@@ -17,8 +16,23 @@ class DispFeedback extends React.Component {
   render() {
     let text2 = (
       <div className={styles.questions}>
-        The true population on the planet was{" "}
-        {this.props.all_true_pop_size[this.props.trialNum - 1]} million.
+        <Text
+          style={{
+            color: "white",
+            fontSize: 25,
+            fontFamily: "Arial",
+            fontWeight: "bold",
+          }}
+        >
+          <Text>The true population on the planet was</Text>
+          <Text style={{ color: "#b3e49d",fontSize: 35, }}>
+            {" "}
+            {this.props.all_true_pop_size[this.props.trialNum - 1]}
+          </Text>
+          <Text> million.</Text>
+        </Text>
+        <br />
+        <br />
         <br />
         <br />
         <br />
@@ -27,33 +41,33 @@ class DispFeedback extends React.Component {
 
     return (
       <div className={styles.cockpitslider}>
-          <View style={stylesSliderRep.header}>
-            <span className={styles.slidertext}>
-              <div>{text2}</div>
-            </span>
-          </View>
-
-          <span className={styles.slider}>
-          <OutcomeSliderBar
-                mu={this.props.trialSgmMu[this.props.trialNum - 1][2]}
-                sgm={this.props.trialSgmMu[this.props.trialNum - 1][1]}
-                value={this.props.all_true_pop_size[this.props.trialNum - 1]}
-                distHeight={this.props.distHeight}
-                getBarHeight={this.handleHeight}
-              />
+        <View style={stylesSliderRep.header}>
+          <span className={styles.slidertext}>
+            <div>{text2}</div>
           </span>
+        </View>
+
+        <span className={styles.slider}>
+          <OutcomeSliderBar
+            mu={this.props.trialSgmMu[this.props.trialNum - 1][2]}
+            sgm={this.props.trialSgmMu[this.props.trialNum - 1][1]}
+            value={this.props.all_true_pop_size[this.props.trialNum - 1]}
+            distHeight={this.props.distHeight}
+            getBarHeight={this.handleHeight}
+          />
+        </span>
       </div>
     );
   }
 }
 export default withRouter(DispFeedback);
 
-const stylesSliderRep= StyleSheet.create({
+const stylesSliderRep = StyleSheet.create({
   container: {
     flex: 1,
   },
   header: {
     width: "100%",
     position: "absolute",
-  }
+  },
 });
