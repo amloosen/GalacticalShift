18c1b9cc0386d9d7353855d991271563ae199e85
Alisa Loosen <a.m.loosen@gmail.com>
2021-12-09 09:57:02 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://galacticalshift-api.osc-fr1.scalingo.io";
+export const API_URL = "https://galacticalshift-api-patientstudy.osc-fr1.scalingo.io";
 // export const API_URL = "http://localhost:5000";
